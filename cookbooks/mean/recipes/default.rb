5936692538a07fed6e7df16436ea8cf69de0caac
Daiki Hirakata <hirakatad@isao.co.jp>
2015-04-09 02:36:06 +0900

    change cookbooks and change phpStorm setting.

--- cookbooks/mean/recipes/default.rb
+++ cookbooks/mean/recipes/default.rb
@@ -22,29 +22,21 @@ nodejs_npm 'grunt-cli'
 nodejs_npm 'yo'
 nodejs_npm 'bower'
 
-# %w{grunt-cli yo bower}.each do |p|
-#   execute p do
-#     command 'npm install -g ' + p
-#  not_if "npm -g ls 2> /dev/null | grep '^[├└]─[─┬] #{p}@'"
-#   end
-# end
-
 service 'apache2' do
   action :stop
 end
 
 execute 'npm package install' do
-    command "su vagrant -l -c 'cd #{node[:app_root]}; npm install'"
+    command "su vagrant -l -c 'cd #{node[:app_root]}; npm install --silent'"
 end
 
 execute 'bower package install' do
     command "su vagrant -l -c 'cd #{node[:app_root]}; bower install'"
 end
 
-# bash "npm i and bower i" do
-#   user 'vagrant'
-#   code <<-EOS
-#   cd #{node[:app_root]}; npm install
-#   cd #{node[:app_root]}; bower install
-#   EOS
-# end
+template "/home/vagrant/.bash_profile" do
+  owner "vagrant"
+  group "vagrant"
+  mode 0644
+  source ".bash_profile"
+end
